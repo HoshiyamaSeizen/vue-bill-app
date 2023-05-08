8a2c42eaf27e804bedc5ddaf1a22de46e6428bfa
HoshiyamaSeizen <nazarafanasyev@gmail.com>
2023-05-08 15:11:41 +0300

    add router

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,9 +1,18 @@
-<script setup>
-</script>
+<script setup></script>
 
 <template>
-  <button type="button" class="btn btn-primary">Hello World</button>
+	<div class="container">
+		<button type="button" class="btn btn-primary">Hello World</button>
+		<router-link to="/">Go to Home</router-link>
+		<router-link to="/about">Go to About</router-link>
+
+		<router-view></router-view>
+	</div>
 </template>
 
 <style scoped lang="sass">
+*
+  display: block
+  margin: auto
+  text-align: center
 </style>
