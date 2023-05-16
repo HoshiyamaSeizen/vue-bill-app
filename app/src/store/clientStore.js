1303deffc8367e50c8ef97da35288b7ae95e4628
HoshiyamaSeizen <nazarafanasyev@gmail.com>
2023-05-16 12:12:05 +0300

    add client object factory

--- app/src/store/clientStore.js
+++ app/src/store/clientStore.js
@@ -1,13 +1,15 @@
+const Client = (id, name) => ({id, name})
+
 export default {
   namespaced: true,
   state() {
     return {
-      clients: [...Array(10).keys()].map(id => { return {id, name: "John Doe "+(id+1)}})
+      clients: [...Array(10).keys()].map(id => Client(id, "John Doe "+(id+1)))
     }
   },
   mutations: {
     add(state, {name}) {
-      state.clients.push({id: state.clients[state.clients.length-1]?.id+1 || 0, name});
+      state.clients.push(Client(state.clients[state.clients.length-1]?.id+1 || 0, name));
     },
 	edit(state, {id, name}){
 	  state.clients.find(c => c.id === id).name = name || `Client ${id+1}`;
